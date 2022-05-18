8d9ebfd876b8bf48b515f958d1bb7eb5e75e56bb
UserD12 <105378359+UserD12@users.noreply.github.com>
2022-05-18 19:30:59 +0300

    Update ping_pong.py
    v2.0 С фоном и ракетками (ракетки движутся)

--- ping_pong.py
+++ ping_pong.py
@@ -6,8 +6,39 @@ background = transform.scale(image.load('background.png'), (700, 500))
 
 clock = time.Clock()
 FPS = 60
+
 game = True
 
+win_height = 500
+
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, player_speed, player_width, player_height):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image), (player_width, player_height))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+class Player(GameSprite):
+
+    def update_r(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < win_height - 155:
+            self.rect.y += self.speed
+    def update_l(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < win_height - 155:
+            self.rect.y += self.speed
+
+player1 = Player('raketka.png', 30, 200, 4, 20, 150)
+player2 = Player('raketka.png', 650, 200, 4, 20, 150)
 
 while game:
     window.blit(background, (0,0))
@@ -15,6 +46,10 @@ while game:
     for e in event.get():
         if e.type == QUIT:
             game = False#задай фон сцены
+    player1.update_l()
+    player1.reset()
+    player2.update_r()
+    player2.reset()
 
     display.update()
     clock.tick(FPS)
